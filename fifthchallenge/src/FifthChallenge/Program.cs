eaa93fb0a8b0a70fc1ce32f4c538067c00e39c43
evaggelia-maria-spyrou <evaggelia.mar.spyrou@gmail.com>
2023-06-29 10:07:56 +0300

    modified files

--- fifthchallenge/src/FifthChallenge/Program.cs
+++ fifthchallenge/src/FifthChallenge/Program.cs
@@ -1,1 +1,40 @@
-﻿
+﻿namespace FifthChallenge{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var url = "https://adventofcode.com/2022/day/5/input";
+            var cookies = "sessionCookies";
+            var httpClient = new HttpClient();
+
+            httpClient.DefaultRequestHeaders.Add("Cookie", $"session={cookies}");
+            var response = await httpClient.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var data = await response.Content.ReadAsStringAsync();
+                List<string> dataList = new List<string>(data.Split("\n"));
+                MakeTheTables makeTheTables=new MakeTheTables();
+                SequenceOfTheLastCrates seq=new SequenceOfTheLastCrates();
+                //Create the table of stacks of crates
+                var tableOfCrates=makeTheTables.MakeTheTableOfTheCrates(dataList);
+                //Create the table of steps and moves of crates
+                var tableOfSteps=makeTheTables.MakeTheTableOfSteps(dataList);
+                //Task1
+                seq.TaskOne(tableOfCrates,tableOfSteps);
+                //Task2
+                seq.TaskTwo(tableOfCrates,tableOfSteps);
+
+
+
+
+              
+               
+            }
+            else
+            {
+                Console.WriteLine(response.StatusCode);
+            }
+        }
+    }
+}
